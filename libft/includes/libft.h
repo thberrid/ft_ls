f880f6c08a09ac5b61d74e3114e10654b8c23425
Thibault BERRIDO <thberrid@e1r1p13.42.fr>
2020-01-06 05:24:25 +0100

    libft, extract is_case

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -68,6 +68,8 @@ int				ft_ischarset(int c, char *charset);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
+int				ft_islower(int c);
+int				ft_isupper(int c);
 int				ft_isspace(int c);
 int				ft_isprint(int c);
 int				ft_toupper(int c);
