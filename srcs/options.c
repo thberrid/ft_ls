79118d96e337cb483af1a5321ff70c2966524a2d
Thibault BERRIDO <thberrid@e1r1p13.42.fr>
2020-01-06 05:49:45 +0100

    add usage

--- srcs/options.c
+++ srcs/options.c
@@ -12,9 +12,20 @@
 
 #include <ft_ls.h>
 
-int		options_set(int ac, char **av, t_options *options)
+static int		print_usage_and_quit(char invalid_char)
 {
-	int		retrn;
+	ft_putstr("ft_ls: illegal option -- ");
+	ft_putchar(invalid_char);
+	ft_putchar('\n');
+	ft_putstr("usage: ./ft_ls [-");
+	ft_putstr(FLAGS_ALLOWD);
+	ft_putendl("] [file ...]");
+	return (1);
+}
+
+int				options_set(int ac, char **av, t_options *options)
+{
+	char		retrn;
 
 	retrn = 0;
 	ft_bzero(options, sizeof(t_options));
@@ -24,6 +35,8 @@ int		options_set(int ac, char **av, t_options *options)
 		options->path = ft_strdup("./");
 //	else
 //		options->path = ft_strjoin(av[1], "/");
+	if (retrn)
+		return (print_usage_and_quit(retrn));
 	if (!options->path)
 		return (1);
 	return (0);
